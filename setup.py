cbbad4534bc48817318fb0e7e3367eb52d46f53a
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-10-08 08:52:42 +0200

    Adjust deps to work with development version of Mopidy

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'setuptools',
-        'Mopidy >= 0.16',
+        'Mopidy >= 0.16a0',
         'Pykka >= 1.1',
     ],
     test_suite='nose.collector',
