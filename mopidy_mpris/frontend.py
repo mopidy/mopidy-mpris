9fdae9fb423e7902ba577892194b7e6189206bc6
Stein Magnus Jodal <stein.magnus@jodal.no>
2018-12-02 22:17:10 +0100

    Reduce boilerplate in events handling

--- mopidy_mpris/frontend.py
+++ mopidy_mpris/frontend.py
@@ -35,94 +35,60 @@ class MprisFrontend(pykka.ThreadingActor, CoreListener):
             self.mpris = None
         logger.debug('Removed MPRIS object from D-Bus connection')
 
-    def track_playback_paused(self, tl_track, time_position):
-        logger.debug('Received track_playback_paused event')
+    def on_event(self, event, **kwargs):
+        logger.debug('Received %s event', event)
         if self.mpris is None:
             return
+        return super(MprisFrontend, self).on_event(event, **kwargs)
+
+    def track_playback_paused(self, tl_track, time_position):
         _emit_properties_changed(self.mpris.player, ['PlaybackStatus'])
 
     def track_playback_resumed(self, tl_track, time_position):
-        logger.debug('Received track_playback_resumed event')
-        if self.mpris is None:
-            return
         _emit_properties_changed(self.mpris.player, ['PlaybackStatus'])
 
     def track_playback_started(self, tl_track):
-        logger.debug('Received track_playback_started event')
-        if self.mpris is None:
-            return
         _emit_properties_changed(
             self.mpris.player, ['PlaybackStatus', 'Metadata'])
 
     def track_playback_ended(self, tl_track, time_position):
-        logger.debug('Received track_playback_ended event')
-        if self.mpris is None:
-            return
         _emit_properties_changed(
             self.mpris.player, ['PlaybackStatus', 'Metadata'])
 
     def playback_state_changed(self, old_state, new_state):
-        logger.debug('Received playback_state_changed event')
-        if self.mpris is None:
-            return
         _emit_properties_changed(
             self.mpris.player, ['PlaybackStatus', 'Metadata'])
 
     def tracklist_changed(self):
-        logger.debug('Received tracklist_changed event')
         pass  # TODO Implement if adding tracklist support
 
     def playlists_loaded(self):
-        logger.debug('Received playlists_loaded event')
-        if self.mpris is None:
-            return
         _emit_properties_changed(self.mpris.playlists, ['PlaylistCount'])
 
     def playlist_changed(self, playlist):
-        logger.debug('Received playlist_changed event')
-        if self.mpris is None:
-            return
         playlist_id = get_playlist_id(playlist.uri)
         playlist = (playlist_id, playlist.name, '')
         self.mpris.playlists.PlaylistChanged(playlist)
 
     def playlist_deleted(self, uri):
-        logger.debug('Received playlist_deleted event')
-        if self.mpris is None:
-            return
         _emit_properties_changed(self.mpris.playlists, ['PlaylistCount'])
 
     def options_changed(self):
-        logger.debug('Received options_changed event')
-        if self.mpris is None:
-            return
         _emit_properties_changed(
             self.mpris.player,
             ['LoopStatus', 'Shuffle', 'CanGoPrevious', 'CanGoNext'])
 
     def volume_changed(self, volume):
-        logger.debug('Received volume_changed event')
-        if self.mpris is None:
-            return
         _emit_properties_changed(self.mpris.player, ['Volume'])
 
     def mute_changed(self, mute):
-        logger.debug('Received mute_changed event')
-        if self.mpris is None:
-            return
         _emit_properties_changed(self.mpris.player, ['Volume'])
 
     def seeked(self, time_position):
-        logger.debug('Received seeked event')
-        if self.mpris is None:
-            return
         time_position_in_microseconds = time_position * 1000
         self.mpris.player.Seeked(time_position_in_microseconds)
 
     def stream_title_changed(self, title):
-        logger.debug('Received stream_title_changed event')
-        if self.mpris is None:
-            return
         _emit_properties_changed(self.mpris.player, ['Metadata'])
 
 
