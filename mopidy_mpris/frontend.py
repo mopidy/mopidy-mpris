9cc7168bd2af8678fed5548dddafecfbd32c7de8
Stein Magnus Jodal <stein.magnus@jodal.no>
2018-12-02 15:08:00 +0100

    Log all Mopidy events

--- mopidy_mpris/frontend.py
+++ mopidy_mpris/frontend.py
@@ -61,18 +61,13 @@ class MprisFrontend(pykka.ThreadingActor, CoreListener):
         _emit_properties_changed(
             self.mpris.player, ['PlaybackStatus', 'Metadata'])
 
-    def volume_changed(self, volume):
-        logger.debug('Received volume_changed event')
-        if self.mpris is None:
-            return
-        _emit_properties_changed(self.mpris.player, ['Volume'])
+    def playback_state_changed(self, old_state, new_state):
+        logger.debug('Received playback_state_changed event')
+        pass  # TODO
 
-    def seeked(self, time_position):
-        logger.debug('Received seeked event')
-        if self.mpris is None:
-            return
-        time_position_in_microseconds = time_position * 1000
-        self.mpris.player.Seeked(time_position_in_microseconds)
+    def tracklist_changed(self):
+        logger.debug('Received tracklist_changed event')
+        pass  # TODO
 
     def playlists_loaded(self):
         logger.debug('Received playlists_loaded event')
@@ -85,6 +80,35 @@ class MprisFrontend(pykka.ThreadingActor, CoreListener):
         playlist_id = get_playlist_id(playlist.uri)
         playlist = (playlist_id, playlist.name, '')
         self.mpris.playlists.PlaylistChanged(playlist)
+
+    def playlist_deleted(self, uri):
+        logger.debug('Received playlist_deleted event')
+        pass  # TODO
+
+    def options_changed(self):
+        logger.debug('Received options_changed event')
+        pass  # TODO
+
+    def volume_changed(self, volume):
+        logger.debug('Received volume_changed event')
+        if self.mpris is None:
+            return
+        _emit_properties_changed(self.mpris.player, ['Volume'])
+
+    def mute_changed(self, mute):
+        logger.debug('Received mute_changed event')
+        pass  # TODO
+
+    def seeked(self, time_position):
+        logger.debug('Received seeked event')
+        if self.mpris is None:
+            return
+        time_position_in_microseconds = time_position * 1000
+        self.mpris.player.Seeked(time_position_in_microseconds)
+
+    def stream_title_changed(self, title):
+        logger.debug('Received stream_title_changed event')
+        pass  # TODO
 
 
 def _emit_properties_changed(interface, changed_properties):
