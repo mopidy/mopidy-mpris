aca61ee4c8a86711b36d37ec9490bc758508a198
Stein Magnus Jodal <stein.magnus@jodal.no>
2025-11-26 22:17:35 +0100

    chore: Port from unittest to pytest

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,32 +1,32 @@
-import unittest
 from unittest import mock
 
 from mopidy_mpris import Extension
 from mopidy_mpris import frontend as frontend_lib
 
 
-class ExtensionTest(unittest.TestCase):
-    def test_get_default_config(self):
-        ext = Extension()
+def test_get_default_config():
+    ext = Extension()
 
-        config = ext.get_default_config()
+    config = ext.get_default_config()
 
-        assert "[mpris]" in config
-        assert "enabled = true" in config
-        assert "bus_type = session" in config
+    assert "[mpris]" in config
+    assert "enabled = true" in config
+    assert "bus_type = session" in config
 
-    def test_get_config_schema(self):
-        ext = Extension()
 
-        schema = ext.get_config_schema()
+def test_get_config_schema():
+    ext = Extension()
 
-        assert "desktop_file" in schema
-        assert "bus_type" in schema
+    schema = ext.get_config_schema()
 
-    def test_get_frontend_classes(self):
-        ext = Extension()
-        registry = mock.Mock()
+    assert "desktop_file" in schema
+    assert "bus_type" in schema
 
-        ext.setup(registry)
 
-        registry.add.assert_called_once_with("frontend", frontend_lib.MprisFrontend)
+def test_get_frontend_classes():
+    ext = Extension()
+    registry = mock.Mock()
+
+    ext.setup(registry)
+
+    registry.add.assert_called_once_with("frontend", frontend_lib.MprisFrontend)
